cba43914312a68b6b3003002e7ebe39d4ad0382c
“EppuKos” <eppukoskiranta@kamk.fi>
2020-09-24 13:58:28 +0300

    Added prefab for car. Also added Car.cs and CarController.cs

--- DrivingSimulation/Assets/Scripts/Car.cs
+++ DrivingSimulation/Assets/Scripts/Car.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
+using TMPro;
 using UnityEngine;
 
 public class Car : MonoBehaviour
@@ -8,7 +10,7 @@ public class Car : MonoBehaviour
     //Light stuff
     enum LightMode
     {
-        DEFAULT,
+        DEFAULT = 0,
         ON,
         MIST,
 
@@ -16,14 +18,15 @@ public class Car : MonoBehaviour
 
     enum Lights
     {
-        ON = 0x01,
-        LONG = 0x02,
-        LEFTBLINKER = 0x03,
-        RIGHTBLINKER = 0x04,
+        DEFAULT = 0b_1,
+        LONG = 0b_10,
+        LEFTBLINKER = 0b_100,
+        RIGHTBLINKER = 0b_1000,
     }
 
-    LightMode lightMode;
-    uint lights;
+    LightMode lightMode = LightMode.DEFAULT;
+    uint lights = (uint)Lights.DEFAULT;
+    Light[] lightComponents;
 
     //Wipers
     enum Wipers
@@ -38,36 +41,95 @@ public class Car : MonoBehaviour
 
     Wipers wipers;
 
+    //Wheel stuff
+    public enum CarAxis
+    {
+        FRONT = 0,
+        REAR,
+    }
+
+
+    /// <summary>
+    /// Is car Front-wheel drive, Rear-wheel drive or All-wheel drive
+    /// </summary>
+    public enum WheelDrive
+    {
+        FRONTWHEEL = 0,
+        REARWHEEL,
+        ALLWHEEL,
+    }
+
+
+    public struct Wheel
+    {
+        public GameObject model;
+        public WheelCollider wheel;
+        public CarAxis axis;
+    }
+
+    public WheelDrive wheelMode = WheelDrive.FRONTWHEEL;
+
+    [SerializeField]
+    public Wheel[] wheels = new Wheel[4];
+
+
+    //Automatic or Manual
+    public enum GearSystem
+    {
+        AUTOMATIC,
+        MANUAL,
+    }
+
+    GearSystem gearSystem = GearSystem.AUTOMATIC;
+
+
+    public enum AutomaticGearMode
+    {
+        PARK = 0,
+        REVERSE,
+        NEUTRAL,
+        DRIVE,
+    }
+
+    AutomaticGearMode automaticGearMode = AutomaticGearMode.PARK;
+
     //Motor stuff
     bool automatic = true;
-    bool handbreak = false;
+    bool handbrake = false;
+    bool engineOn = false;
+    float maxSpeed = 250f;
+    float maxRPM = 6500f;
     float fuel = 70.0f;
     float roundsPerMinute = 0f;
-    float rpmReduction = 200f;
-    float speed = 0f; //km/h cause fuck miles
+    float rpmReduction = 1000f;
+    float speed = 0f; //    km/h
     float horsePower = 200f;
     float motorTorque = 0f;
+
 
     int activeGear = 0;
     int maxGear = 5;
     float[] gear = { 3.166f, 1.882f, 1.296f, 0.972f, 0.738f, 0.600f };
+
+    float reverseGear = 2.6f;
     
     float tyreCircumference = 1.99271f;
     float differentialRatio = 4.1f;
 
-    //testing input stuff
-    float gasPedal = -3000f;
-    float breakPedal = 0.0f;
-    float steeringWheel = 0f;
+    public float brakeTorque = 1620f;
 
     float maxWheelAngle = 35f; //in euler angles
 
 
+    //Air resistance
+    float Cd = 0.35f; //coefficient
+    float density = 1f;
+    float dragArea = 1.5f; //squaremeters (estimate)
+     
 
-
-    //Components
-    public WheelCollider wheelFrontLeft, wheelFrontRight, wheelRearLeft, wheelRearRight;
-    public GameObject wheelFrontLeftGo, wheelFrontRightGo, wheelRearLeftGo, wheelRearRightGo; 
+    //References
+    public CarController controller;
+    public Rigidbody rb;
 
     //Extras
     //like radio feat. skirmish beats
@@ -75,90 +137,330 @@ public class Car : MonoBehaviour
 
 
 
-
     void Start()
     {
-
+        SetupCar();
     }
-
 
     void Update()
     {
-        ProcessInputKeyboard();
-        AddGas();
-        SwitchGearAutomatic();
-        Steer();
-        CalculateTorque();
-        DebugSomeValues();
-        ApplyTorqueToWheels();
+        if (!engineOn)
+            StartEngine();
+
+        if (engineOn)
+        {
+            CarUpdate();
+        }
+    }
+
+    void SetupCar()
+    {
+        //Setup controller
+        if (!controller)
+            controller = this.gameObject.GetComponent<CarController>();
+
+        if (!rb)
+            rb = this.gameObject.GetComponent<Rigidbody>();
+
+
+        //Setup wheels
+        SetupWheels();
+
+
+        //SetupLights
+        SetupLights();
     }
 
 
-    void ProcessInputKeyboard()
+    void SetupWheels()
     {
-        if (Input.GetKeyUp(KeyCode.W))
+        wheels[0] = new Wheel(); //FrontLeft
+        wheels[0].axis = CarAxis.FRONT;
+        wheels[0].model = this.transform.GetChild(3).GetChild(18).gameObject;
+        wheels[0].wheel = this.transform.GetChild(3).GetChild(25).GetChild(0).GetComponent<WheelCollider>();
+        wheels[1] = new Wheel(); //FrontRight
+        wheels[1].axis = CarAxis.FRONT;
+        wheels[1].model = this.transform.GetChild(3).GetChild(19).gameObject;
+        wheels[1].wheel = this.transform.GetChild(3).GetChild(25).GetChild(1).GetComponent<WheelCollider>();
+        wheels[2] = new Wheel(); //RearLeft
+        wheels[2].axis = CarAxis.REAR;
+        wheels[2].model = this.transform.GetChild(3).GetChild(20).gameObject;
+        wheels[2].wheel = this.transform.GetChild(3).GetChild(25).GetChild(2).GetComponent<WheelCollider>();
+        wheels[3] = new Wheel(); //RearRight
+        wheels[3].axis = CarAxis.REAR;
+        wheels[3].model = this.transform.GetChild(3).GetChild(21).gameObject;
+        wheels[3].wheel = this.transform.GetChild(3).GetChild(25).GetChild(3).GetComponent<WheelCollider>();
+
+
+        tyreCircumference = wheels[0].wheel.radius * Mathf.PI * 2;
+
+
+        wheels[0].wheel.ConfigureVehicleSubsteps(5.0f, 5, 2);
+    }
+
+
+
+    void SetupLights()
+    {
+        //0 LeftDefaultLight, 1 RightDefaultLight, 2 LeftLongLight, 
+        //3 RightLongLight, 4 LeftBrakeLight, 5 RightBrakeLight, 
+        //6 LeftFrontBlinker, 7 LeftRearBlinker, 8 RightFrontBlinker, 9 RightRearBlinker
+
+        lightComponents = this.transform.GetComponentsInChildren<Light>();
+
+
+    }
+
+
+    void CarUpdate()
+    {
+        //Take all inputs from controller
+        /*
+        
+
+        SwitchDriveMode()
+
+        Gas()
         {
-            gasPedal = -3000.0f;
+            Read gasPedal value 0 - 1
+            Increase roundsPerMinute with gasPedal
+                
+            SwitchGear();
+            
+            CalculateTorque()
+            ApplyTorque()
+            
+            CalculateForcesAgainstMovement()
+            {
+                AirResistance
+                InternalExternal friction (includes engine friction, friction with gears, wheels friction)
+                
+            }
+
+
+            ReduceRoundsPerMinute()
         }
-        if (Input.GetKey(KeyCode.W))
+
+        Brake()
         {
-            gasPedal = 3000.0f;
+            Read brakePedal value 0 - 1
+            ApplyBrakeTorque to wheels
         }
-        if (Input.GetKey(KeyCode.D))
+
+
+        Handbrake()
         {
-            if (steeringWheel < 100f)
-                steeringWheel += 30f * Time.deltaTime;
+            check if handbrake on
+            applybraketorque
         }
-        if (Input.GetKey(KeyCode.A))
+
+        Steer()
         {
-            if (steeringWheel > -100)
-                steeringWheel -= 30f * Time.deltaTime;
+            Read steeringWheel value -1 to 1
+            apply angle to wheels
         }
+        
+        Inputs()
+        {
+            Wipers/Turn signals
+        }
+        
+
+        DebugValues()
+
+         */
+
+
+        //Gas
+        SwitchDriveMode();
+        AddGas();
+        Brake();
+        Handbrake();
+        Steer();
+        CalculateSpeed();
+        WidgetUpdate();
+        DebugSomeValues();
+    }
+
+    void StartEngine()
+    {
+        if (controller.startEngine)
+        {
+            engineOn = true;
+            controller.startEngine = false;
+
+            lightComponents[0].enabled = true;
+            lightComponents[1].enabled = true;
+            Debug.Log("Started Engine!! VROM VROM!");
+            //play audio
+        }
+    }
+
+
+    void Gas()
+    {
+        //increase rounds if gas pedal is pressed
+        if (controller.gasPedal > 0 && speed < maxSpeed && engineOn)
+        {
+            roundsPerMinute += controller.gasPedal * gear[activeGear] * 200f * Time.deltaTime;
+        }
+        else
+        {
+            if (roundsPerMinute > 0 && wheels[0].wheel.rpm != 0)
+                roundsPerMinute -= (rpmReduction * Time.deltaTime);
+            else if (roundsPerMinute > 0 && wheels[0].wheel.rpm == 0)
+                roundsPerMinute -= rpmReduction * 1.5f * Time.deltaTime;
+            else
+                roundsPerMinute = 0;
+        }
+
     }
 
 
     void AddGas()
     {
-        //increase rounds if gas pedal is pressed
-        if (gasPedal > -3000.0f)
-        {
-            roundsPerMinute += Mathf.InverseLerp(-3000f, 3000f, gasPedal) * gear[activeGear] * 200f * (float)Time.deltaTime;
-        }
-        else
-        {
-            if (roundsPerMinute > 0)
-                roundsPerMinute -= rpmReduction * (float)Time.deltaTime;
-        }
 
+        float gas = controller.gasPedal;
+        if (roundsPerMinute < maxRPM)
+            roundsPerMinute += gas * 10000f * Time.deltaTime;
+
+        if (automatic)
+            SwitchGearAutomatic();
+
+        CalculateTorque();
+        ApplyTorqueToWheels();
+
+        ReduceRoundsPerMinute();
     }
 
+
+    /// <summary>
+    /// Air resistance, engine friction, wheels friction, gears friction
+    /// </summary>
+    float CalculateFrictionForces()
+    {
+        float totalFriction = 0;
+
+        rb.AddForce(-this.transform.forward * AirResistance());
+
+        //TODO calculate real values
+
+        totalFriction += 0f;
+
+        totalFriction += 100 * ((1 / gear[activeGear]) * (roundsPerMinute / speed));
+
+        
+        return totalFriction;
+    }
+
+
+    void ReduceRoundsPerMinute()
+    {
+        if (roundsPerMinute > 0)
+            roundsPerMinute -= CalculateFrictionForces() * Time.deltaTime;
+        else if (roundsPerMinute < 0)
+            roundsPerMinute = 0;
+    }
 
     void Steer()
     {
-        float turnProgress = Mathf.InverseLerp(-100, 100, steeringWheel);
+        float turnProgress = Mathf.InverseLerp(-1, 1, controller.steeringWheel);
         float wheelAngle = Mathf.Lerp(-maxWheelAngle, maxWheelAngle, turnProgress);
 
         //Turn wheel component
-        wheelFrontLeft.steerAngle = wheelAngle;
-        wheelFrontRight.steerAngle = wheelAngle;
+        foreach (Wheel w in wheels)
+        {
+            if (w.axis == CarAxis.FRONT)
+            {
+                w.wheel.steerAngle = wheelAngle; //wheelcomponent
+                w.model.transform.localRotation = Quaternion.Euler(0, wheelAngle, 0); //model (visual only)
+            }
+        }
 
 
-        //turn actual wheel (visual only)
-        wheelFrontLeftGo.transform.localRotation = Quaternion.Euler(0, wheelAngle, 0);
-        wheelFrontRightGo.transform.localRotation = Quaternion.Euler(0, wheelAngle, 0);
+
     }
 
 
-    void Break()
+    void SwitchDriveMode()
     {
+        if (controller.brakePedal > 0)
+        {
+            if (controller.gearDown)
+            {
+                int activeGearMode = (int)automaticGearMode;
 
+                if (activeGearMode != 0)
+                    activeGearMode--;
+
+                automaticGearMode = (AutomaticGearMode)activeGearMode;
+
+                controller.gearDown = false;
+            }
+            else if (controller.gearUp)
+            {
+                int activeGearMode = (int)automaticGearMode;
+
+                if (activeGearMode != 3)
+                    activeGearMode++;
+
+                automaticGearMode = (AutomaticGearMode)activeGearMode;
+
+                controller.gearUp = false;
+            }
+        }
+
+
+        Debug.Log("ActiveGearMode: " + automaticGearMode);
+    }
+
+    void CalculateSpeed()
+    {
+        speed = rb.velocity.magnitude * 3.6f;
+
+    }
+
+    float AirResistance()
+    {
+        return 0;
+        //return 0.5f * density * Mathf.Pow(speed, 2) * Cd * dragArea;
+    }
+
+
+    void Handbrake()
+    {
+        if (handbrake)
+        {
+            foreach (Wheel w in wheels)
+            {
+                w.wheel.brakeTorque = 10000f;
+            }
+        }
+    }
+
+    void Brake()
+    {
+        if (controller.brakePedal > 0)
+        {
+            foreach (Wheel w in wheels)
+            {
+                w.wheel.brakeTorque = this.brakeTorque;
+            }
+        }
+        else
+        {
+            foreach (Wheel w in wheels)
+            {
+                w.wheel.brakeTorque = 0f;
+            }
+        }
     }
 
     void SwitchGearAutomatic()
     {
         float threshold = 2200f;
 
-        float thresholdIncrease = Mathf.InverseLerp(-3000f, 3000f, gasPedal) * 1000f;
+        float thresholdIncrease = Mathf.InverseLerp(-3000f, 3000f, controller.gasPedal) * 1000f;
 
         threshold += thresholdIncrease;
 
@@ -190,87 +492,166 @@ public class Car : MonoBehaviour
 
     void CalculateTorque()
     {
-        if (roundsPerMinute != 0)
-            motorTorque = horsePower / (roundsPerMinute / 5252);
-        else
-            motorTorque = 0;
+        if (engineOn)
+        {
+            if (roundsPerMinute != 0 && controller.gasPedal != 0)
+                motorTorque = horsePower / (roundsPerMinute / 5252);
+            else
+                motorTorque = 0;
+        }
 
 
         if (motorTorque < 0)
             motorTorque = 0;
+
     }
 
 
     void DebugSomeValues()
     {
-        //Debug.Log("InverseLerp value: " + Mathf.InverseLerp(-3000f, 3000f, gasPedal));
-        Debug.Log("Gear" + activeGear + "Ratio: " + gear[activeGear]);
-        Debug.Log("RPM:" + roundsPerMinute);
-        Debug.Log("Torque:" + motorTorque);
+        Debug.Log("Light bits: " + Convert.ToString(lights, toBase: 2));
+        //Debug.Log("RPM: " + roundsPerMinute);
+        //Debug.Log("Speed: " + speed);
     }
 
     void ApplyTorqueToWheels()
     {
-        if (gasPedal > -3000f)
+        float motorTorqueDir = 0f;
+        if (automaticGearMode == AutomaticGearMode.DRIVE)
+            motorTorqueDir = this.motorTorque;
+
+        if (automaticGearMode == AutomaticGearMode.REVERSE)
+            motorTorqueDir = -this.motorTorque;
+
+
+        if (controller.gasPedal > 0)
         {
-            wheelFrontLeft.motorTorque = this.motorTorque;
-            wheelFrontRight.motorTorque = this.motorTorque;
+            switch (wheelMode)
+            {
+                case WheelDrive.FRONTWHEEL:
+                    foreach (Wheel w in wheels)
+                    {
+                        if (w.axis == CarAxis.FRONT)
+                        {
+                            w.wheel.motorTorque = motorTorqueDir;
+                        }
+                    }
+                    break;
+                case WheelDrive.REARWHEEL:
+                    foreach (Wheel w in wheels)
+                    {
+                        if (w.axis == CarAxis.REAR)
+                        {
+                            w.wheel.motorTorque = motorTorqueDir;
+                        }
+                    }
+                    break;
+                case WheelDrive.ALLWHEEL:
+                    foreach (Wheel w in wheels)
+                    {
+                        w.wheel.motorTorque = motorTorqueDir;
+                    }
+                    break;
+            }
         }
         else
         {
-            wheelFrontLeft.motorTorque = 0;
-            wheelFrontRight.motorTorque = 0;
+            foreach (Wheel w in wheels)
+            {
+                w.wheel.motorTorque = 0f;
+            }
         }
     }
 
 
-    //void RoundsPerMinute()
-    //{
+    void WidgetUpdate()
+    {
+        //Switch mode //default, on, mist
+        LightSwitchMode();
 
-    //    if (gasPedal > 0)
-    //    {
-    //        roundsPerMinute += Mathf.InverseLerp(-3000f, 3000f, gasPedal) * 1000f * (float)Time.deltaTime;
-    //    }
-
-    //    //Reduction
-    //    if (roundsPerMinute > 0)
-    //    {
-
-    //        if (roundsPerMinute > 2560f)
-    //        {
-    //            if (activeGear < maxGear)
-    //            {
-    //                roundsPerMinute = 1600f;
-    //                activeGear++;
-    //            }
-    //        }
-    //        else if (roundsPerMinute < 1000f)
-    //        {
-    //            if (activeGear > 1)
-    //            {
-    //                roundsPerMinute = 1600f;
-    //                activeGear--;
-    //            }
-    //        }
-
-    //        if (gasPedal < 1)
-    //        {
-    //            roundsPerMinute -= rpmReduction * (float)Time.deltaTime;
-    //        }
-    //    }
+        //Turn lights on/off //blinkers, long lights
+        LightsUpdate();
+    }
 
 
-    //    float perSecond = roundsPerMinute / 60;
-    //    float differentialRounds = perSecond / differentialRatio;
-    //    float gearRounds = differentialRounds / gear[activeGear - 1];
-    //    float metersPerSeconds = gearRounds * tyreCircumference;
+    void LightSwitchMode()
+    {
 
-    //    speed = metersPerSeconds * 3.6f;
+        if (controller.lightsModeLeft)
+        {
+            if (lightMode != LightMode.DEFAULT)
+            {
+                lightMode--;
+            }
 
-    //    Debug.Log("Gear" + activeGear + "Ratio: " + gear[activeGear - 1]);
-    //    Debug.Log("RPM:" + roundsPerMinute);
-    //    Debug.Log("Speed:" + speed);
-    //}
+            controller.lightsModeLeft = false;
+        }
+
+        if (controller.lightsModeRight)
+        {
+            if (lightMode != LightMode.MIST)
+            {
+                lightMode++;
+            }
+
+            controller.lightsModeRight = false;
+        }
+    }
+
+
+    void LightsUpdate()
+    {
+        if (controller.blinkerLeft)
+        {
+            lights |= (uint)Lights.LEFTBLINKER;
+            controller.blinkerLeft = false;
+        }
+
+
+        if (controller.blinkerRight)
+        {
+            lights |= (uint)Lights.RIGHTBLINKER;
+            controller.blinkerRight = false;
+        }
+
+        if (controller.longLights)
+        {
+            if (lightMode == LightMode.ON)
+            { 
+                lights ^= (uint)Lights.DEFAULT;
+                lights ^= (uint)Lights.LONG;
+
+                if ((int)lights >> 0 == 1)
+                {
+                    lightComponents[0].enabled = true;
+                    lightComponents[1].enabled = true;
+                }
+                else
+                {
+                    lightComponents[0].enabled = false;
+                    lightComponents[1].enabled = false;
+                }
+
+                if ((int)lights >> 1 == 1)
+                {
+                    lightComponents[2].enabled = true;
+                    lightComponents[3].enabled = true;
+                }
+                else
+                {
+                    lightComponents[2].enabled = false;
+                    lightComponents[3].enabled = false;
+                }
+            }
+            controller.longLights = false;
+        }
+    }
+
+
+
+
+
+
 
 
 }
---
+++ DrivingSimulation/Assets/Scripts/CarController.cs
@@ -0,0 +1,221 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text;
+
+//Handles all input variables for car
+public class CarController : MonoBehaviour
+{
+    private string actualState;
+    private string activeForces;
+    private string propertiesEdit;
+    private string buttonStatus;
+    private string forcesLabel;
+    string[] activeForceAndEffect;
+
+
+    public float gasPedal = 0f; //from x to y value
+    public float brakePedal = 0f; //from x to y value
+    public float steeringWheel = 0f; //from x to y value
+
+    public bool gearUp = false;
+    public bool gearUpReleased = true;
+    public bool gearDown = false;
+    public bool gearDownReleased = true;
+    public bool startEngine = false;
+    public bool handbrake = false;
+    public bool blinkerLeft = false;
+    public bool blinkerRight = false;
+    public bool longLights = false;
+    public bool lightsModeRight = false;
+    public bool lightsModeLeft = false;
+    public bool wiperUp = false;
+    public bool wiperDown = false;
+
+
+
+    LogitechGSDK.LogiControllerPropertiesData properties;
+
+    private void Start()
+    {
+        InitSteeringWheel();
+    }
+
+
+    //Initializes logitech steering wheel
+    void InitSteeringWheel()
+    {
+        LogitechGSDK.LogiSteeringInitialize(false);
+    }
+
+
+    //Called when application is closed
+    private void OnApplicationQuit()
+    {
+        LogitechGSDK.LogiSteeringShutdown();
+    }
+
+
+    private void Update()
+    {
+        ProcessInput();
+    }
+
+
+    void ProcessInput()
+    {
+        //Logitech Steering wheel car input
+        if (LogitechGSDK.LogiUpdate() && LogitechGSDK.LogiIsConnected(0))
+        {
+            StringBuilder deviceName = new StringBuilder(256);
+            LogitechGSDK.LogiGetFriendlyProductName(0, deviceName, 256);
+            propertiesEdit = "Current Controller : " + deviceName + "\n";
+            propertiesEdit += "Current controller properties : \n\n";
+            LogitechGSDK.LogiControllerPropertiesData actualProperties = new LogitechGSDK.LogiControllerPropertiesData();
+            LogitechGSDK.LogiGetCurrentControllerProperties(0, ref actualProperties);
+
+
+
+            LogitechGSDK.DIJOYSTATE2ENGINES rec;
+            rec = LogitechGSDK.LogiGetStateUnity(0);
+
+            float xaxis = (float)rec.lX;
+            steeringWheel = xaxis / 32000.0f;
+            gasPedal = Mathf.InverseLerp(32000, -32000, rec.lY);
+            brakePedal = Mathf.InverseLerp(32000, -32000, rec.lRz);
+
+
+
+            for (int i = 0; i < 128; i++)
+            {
+                switch (i)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    case 7:
+                        break;
+                    case 8:
+                        break;
+                    case 9:
+                        break;
+                    case 10:
+                        break;
+                    case 11:
+                        break;
+                    case 12:
+                        break;
+                    case 13:
+                        break;
+                    case 14: //start engine
+                        if (rec.rgbButtons[i] == 128)
+                        {
+                            startEngine = true;
+                        }
+                        break;
+                }
+            }
+        }
+
+#if UNITY_EDITOR //Mouse and keyboard input
+
+        float turningRate = 1f;
+
+        if (!LogitechGSDK.LogiIsConnected(0))
+        { 
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                gasPedal = 1f;
+            }
+
+            if (Input.GetKeyUp(KeyCode.W))
+            {
+                gasPedal = 0f;
+            }
+
+
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                brakePedal = 1f;
+            }
+
+            if (Input.GetKeyUp(KeyCode.S))
+            {
+                brakePedal = 0f;
+            }
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                if (steeringWheel > -1)
+                    steeringWheel -= turningRate * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                if (steeringWheel < 1)
+                    steeringWheel += turningRate * Time.deltaTime;
+            }
+
+
+            if (!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+            {
+                steeringWheel = 0;
+            }
+
+
+
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                gearUp = true;
+            }
+
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                gearDown = true;
+            }
+
+            if (Input.GetKeyDown(KeyCode.K))
+            {
+                startEngine = true;
+            }
+
+
+
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                blinkerLeft = true;
+            }
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                blinkerRight = true;
+            }
+
+            if (Input.GetKeyDown(KeyCode.X))
+            {
+                longLights = true;
+            }
+
+
+            if (Input.GetKeyDown(KeyCode.Z))
+            {
+                lightsModeLeft = true;
+            }
+
+            if (Input.GetKeyDown(KeyCode.C))
+            {
+                lightsModeRight = true;
+            }
+        }
+
+#endif
+    }
+}
