b176341f8c44b327c740a9acca0d87acf0b0a7c3
Mäkinen Kia TTV18SP <kiamakinen@kamk.fi>
2020-09-28 14:17:42 +0300

    yes, time to merge this shit

---
+++ DrivingSimulation/Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,36 @@
+﻿using System.CodeDom;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    public bool activated = false;
+    public static GameObject[] CheckpointsList;
+    GameObject player;
+
+    void Start()
+    {
+        CheckpointsList = GameObject.FindGameObjectsWithTag("Checkpoint");
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void ActivateCheckPoint()
+    {
+        foreach (GameObject cp in CheckpointsList)
+        {
+            //TODO so we cant infinitely activate just one checkpoint.
+            //all must be activated before the level can end
+            //also in order I guess /shrug
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            ActivateCheckPoint();
+            Debug.Log("Checkpoint!");
+        }
+    }
+}
---
+++ DrivingSimulation/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    GameObject player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            Debug.Log("Hit");
+        }
+    }
+}
