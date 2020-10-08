9c022d0c72b3c23a1faef963258e5dcef3edf6d5
Juuso Murto <juusomurto@kamk.fi>
2020-10-08 09:02:56 +0300

    fixed ui

---
+++ DrivingSimulation/Assets/Scripts/AnimationController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    Animation myAnimation;
+
+    void Awake()
+    {
+        myAnimation = gameObject.GetComponent<Animation>();
+    }
+
+    void update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("works");
+            myAnimation.Play("selected");
+        }
+    }
+}
