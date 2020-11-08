f77a761209866542016534b91a731defd6e82301
kaapomoi <56117900+kaapomoi@users.noreply.github.com>
2020-11-08 15:51:01 +0200

    Level 2 graphics

---
+++ DrivingSimulation/Assets/Scripts/CameraShake.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    public Car car;
+    // Start is called before the first frame update
+    void Start()
+    {
+        car = GameObject.FindGameObjectWithTag("Car").GetComponent<Car>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
